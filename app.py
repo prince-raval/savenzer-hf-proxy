67c13e4f7caa94f8e2eedf62ea7d467e54b7dccb
Prince Raval <raval.prince03@gmail.com>
2025-10-21 21:02:11 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests, os
 
 app = Flask(__name__)
+CORS(app)
+
 
 HF_API_KEY = os.getenv("HF_API_KEY")
 HF_MODEL = "google/gemma-2-2b-it"
@@ -42,4 +45,5 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
